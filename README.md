72f2538935d84fcd595a795d3224ebcbb4b93e51
Colin Millar <colinpmillar@googlemail.com>
2012-08-23 10:05:00 +0200

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Ignoring-stock-structure
 ========================
 
 The repository for the work towards a presentation on ignoring stock structure in marine fish
+
+
---
+++ StockStructure/R/StockStructure-package.r
@@ -0,0 +1,22 @@
+#' Functions to investige stock structure in fish
+#'
+#' \tabular{ll}{
+#' Package: \tab StockStructure \cr
+#' Type: \tab Package \cr
+#' Version: \tab 0.1 \cr
+#' Date: \tab 2012-08-23 \cr
+#' License: \tab GPL (>= 2) \cr
+#' LazyLoad: \tab yes \cr
+#' }
+#'
+#' @name StockStructure-package
+#' @aliases StockStructure
+#' @docType package
+#' @import Matrix
+#' @title Functions to investige stock structure in fish
+#' @author Colin Millar \email{colin.millar@@jrc.ec.europa.eu}
+#' @author Iago Mosquiera \email{iago.mosquiera@@jrc.ec.europa.eu}
+#' @references none
+#' @keywords package
+#' @seealso \code{\link{paste}}
+NULL
